adf10fc57f96849ac393f867a73641e85f53e689
GMBroadbent <34782917+gmbroadbent@users.noreply.github.com>
2019-11-05 23:13:02 +0000

    First Artifact Commit
    Windows forms application using the News API
    Desktop C# API discontinued, will intergrate in python

---
+++ Artifact/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace SE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.FromDate = new System.Windows.Forms.DateTimePicker();
+            this.ToDate = new System.Windows.Forms.DateTimePicker();
+            this.Label_From = new System.Windows.Forms.Label();
+            this.Label_ToDate = new System.Windows.Forms.Label();
+            this.Check_FromDate = new System.Windows.Forms.CheckBox();
+            this.Check_ToDate = new System.Windows.Forms.CheckBox();
+            this.Label_Country = new System.Windows.Forms.Label();
+            this.Check_Country = new System.Windows.Forms.CheckBox();
+            this.Country = new System.Windows.Forms.ComboBox();
+            this.Catagory = new System.Windows.Forms.ComboBox();
+            this.Check_Catagory = new System.Windows.Forms.CheckBox();
+            this.Label_Catagory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.TextBox.Location = new System.Drawing.Point(316, 198);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(389, 407);
+            this.TextBox.TabIndex = 2;
+            this.TextBox.Text = "";
+            this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // FromDate
+            // 
+            this.FromDate.Location = new System.Drawing.Point(397, 39);
+            this.FromDate.MaxDate = new System.DateTime(2019, 11, 1, 0, 0, 0, 0);
+            this.FromDate.MinDate = new System.DateTime(1850, 1, 1, 0, 0, 0, 0);
+            this.FromDate.Name = "FromDate";
+            this.FromDate.Size = new System.Drawing.Size(200, 20);
+            this.FromDate.TabIndex = 3;
+            this.FromDate.Value = new System.DateTime(2019, 11, 1, 0, 0, 0, 0);
+            // 
+            // ToDate
+            // 
+            this.ToDate.Location = new System.Drawing.Point(397, 71);
+            this.ToDate.Name = "ToDate";
+            this.ToDate.Size = new System.Drawing.Size(200, 20);
+            this.ToDate.TabIndex = 5;
+            // 
+            // Label_From
+            // 
+            this.Label_From.AutoSize = true;
+            this.Label_From.Location = new System.Drawing.Point(313, 44);
+            this.Label_From.Name = "Label_From";
+            this.Label_From.Size = new System.Drawing.Size(56, 13);
+            this.Label_From.TabIndex = 6;
+            this.Label_From.Text = "From Date";
+            // 
+            // Label_ToDate
+            // 
+            this.Label_ToDate.AutoSize = true;
+            this.Label_ToDate.Location = new System.Drawing.Point(313, 72);
+            this.Label_ToDate.Name = "Label_ToDate";
+            this.Label_ToDate.Size = new System.Drawing.Size(46, 13);
+            this.Label_ToDate.TabIndex = 7;
+            this.Label_ToDate.Text = "To Date";
+            // 
+            // Check_FromDate
+            // 
+            this.Check_FromDate.AutoSize = true;
+            this.Check_FromDate.Location = new System.Drawing.Point(376, 44);
+            this.Check_FromDate.Name = "Check_FromDate";
+            this.Check_FromDate.Size = new System.Drawing.Size(15, 14);
+            this.Check_FromDate.TabIndex = 8;
+            this.Check_FromDate.UseVisualStyleBackColor = true;
+            this.Check_FromDate.CheckedChanged += new System.EventHandler(this.Check_FromDate_CheckedChanged);
+            // 
+            // Check_ToDate
+            // 
+            this.Check_ToDate.AutoSize = true;
+            this.Check_ToDate.Location = new System.Drawing.Point(376, 72);
+            this.Check_ToDate.Name = "Check_ToDate";
+            this.Check_ToDate.Size = new System.Drawing.Size(15, 14);
+            this.Check_ToDate.TabIndex = 9;
+            this.Check_ToDate.UseVisualStyleBackColor = true;
+            this.Check_ToDate.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // Label_Country
+            // 
+            this.Label_Country.AutoSize = true;
+            this.Label_Country.Location = new System.Drawing.Point(316, 104);
+            this.Label_Country.Name = "Label_Country";
+            this.Label_Country.Size = new System.Drawing.Size(43, 13);
+            this.Label_Country.TabIndex = 10;
+            this.Label_Country.Text = "Country";
+            // 
+            // Check_Country
+            // 
+            this.Check_Country.AutoSize = true;
+            this.Check_Country.Location = new System.Drawing.Point(376, 104);
+            this.Check_Country.Name = "Check_Country";
+            this.Check_Country.Size = new System.Drawing.Size(15, 14);
+            this.Check_Country.TabIndex = 11;
+            this.Check_Country.UseVisualStyleBackColor = true;
+            this.Check_Country.CheckedChanged += new System.EventHandler(this.Check_Country_CheckedChanged);
+            // 
+            // Country
+            // 
+            this.Country.FormattingEnabled = true;
+            this.Country.Items.AddRange(new object[] {
+            "Argentina",
+            "Australia",
+            "Austria",
+            "Belgium",
+            "Brazil",
+            "Bulgaria",
+            "Canada",
+            "China",
+            "Colombia",
+            "Cuba",
+            "Czechia",
+            "Egypt",
+            "France",
+            "Germany",
+            "Greece",
+            "Hong Kong",
+            "Hungary",
+            "India",
+            "Indonesia",
+            "Ireland",
+            "Israel",
+            "Italy",
+            "Japan",
+            "Korea",
+            "Latvia",
+            "Lithuania",
+            "Malaysia",
+            "Mexico",
+            "Morocco",
+            "Netherlands",
+            "New Zealand",
+            "Nigeria",
+            "Norway",
+            "Philippines",
+            "Poland",
+            "Portugal",
+            "Romania",
+            "Russia",
+            "Saudi Arabia",
+            "Serbia",
+            "Singapore",
+            "Slovakia",
+            "Slovenia",
+            "South Africa",
+            "Sweden",
+            "Switzerland",
+            "Taiwan",
+            "Thailand",
+            "Turkey",
+            "UAE",
+            "UK",
+            "Ukraine",
+            "US",
+            "Venezuela"});
+            this.Country.Location = new System.Drawing.Point(398, 104);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(121, 21);
+            this.Country.Sorted = true;
+            this.Country.TabIndex = 12;
+            // 
+            // Catagory
+            // 
+            this.Catagory.FormattingEnabled = true;
+            this.Catagory.Items.AddRange(new object[] {
+            "Business",
+            "Entertainment",
+            "General",
+            "Health",
+            "Science",
+            "Sports",
+            "Technology"});
+            this.Catagory.Location = new System.Drawing.Point(398, 133);
+            this.Catagory.Name = "Catagory";
+            this.Catagory.Size = new System.Drawing.Size(121, 21);
+            this.Catagory.Sorted = true;
+            this.Catagory.TabIndex = 15;
+            // 
+            // Check_Catagory
+            // 
+            this.Check_Catagory.AutoSize = true;
+            this.Check_Catagory.Location = new System.Drawing.Point(376, 133);
+            this.Check_Catagory.Name = "Check_Catagory";
+            this.Check_Catagory.Size = new System.Drawing.Size(15, 14);
+            this.Check_Catagory.TabIndex = 14;
+            this.Check_Catagory.UseVisualStyleBackColor = true;
+            // 
+            // Label_Catagory
+            // 
+            this.Label_Catagory.AutoSize = true;
+            this.Label_Catagory.Location = new System.Drawing.Point(316, 133);
+            this.Label_Catagory.Name = "Label_Catagory";
+            this.Label_Catagory.Size = new System.Drawing.Size(49, 13);
+            this.Label_Catagory.TabIndex = 13;
+            this.Label_Catagory.Text = "Catagory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 617);
+            this.Controls.Add(this.Catagory);
+            this.Controls.Add(this.Check_Catagory);
+            this.Controls.Add(this.Label_Catagory);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.Check_Country);
+            this.Controls.Add(this.Label_Country);
+            this.Controls.Add(this.Check_ToDate);
+            this.Controls.Add(this.Check_FromDate);
+            this.Controls.Add(this.Label_ToDate);
+            this.Controls.Add(this.Label_From);
+            this.Controls.Add(this.ToDate);
+            this.Controls.Add(this.FromDate);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.DateTimePicker FromDate;
+        private System.Windows.Forms.DateTimePicker ToDate;
+        private System.Windows.Forms.Label Label_From;
+        private System.Windows.Forms.Label Label_ToDate;
+        private System.Windows.Forms.CheckBox Check_FromDate;
+        private System.Windows.Forms.CheckBox Check_ToDate;
+        private System.Windows.Forms.Label Label_Country;
+        private System.Windows.Forms.CheckBox Check_Country;
+        private System.Windows.Forms.ComboBox Country;
+        private System.Windows.Forms.ComboBox Catagory;
+        private System.Windows.Forms.CheckBox Check_Catagory;
+        private System.Windows.Forms.Label Label_Catagory;
+    }
+}
+
---
+++ Artifact/Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// PM> Install-Package NewsAPI
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+
+// PM> Install-Package SpotifyAPI.Web
+// PM> Install-Package SpotifyAPI.Web.Auth
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+
+
+namespace SE
+{
+    public partial class Form1 : Form
+    {
+
+        private NewsApiClient APIn;
+        private SpotifyWebAPI APIs;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            label1.Text = "Program Start";
+
+            SetDefaults();
+
+
+            if (!ValidateNewsAPI() || !ValidateSpotiAPI())
+            {
+
+                TextBox.Text = "News API failed validation\nCheck credentials";
+
+                Disable();
+
+            }
+
+            spotifyTest();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public bool ValidateNewsAPI()
+        {
+            Console.WriteLine("Validating News API");
+            label1.Text = label1.Text + "\nValidating News API";
+
+            string API_key = "";
+            string path = FindCredentials();
+
+            label1.Text += "\nAPI key path: " + path;
+
+            if (File.Exists(path))
+            {
+                API_key = File.ReadLines(path).Skip(0).Take(1).First();
+                label1.Text = label1.Text + "\nAPI Key aquired";
+            }
+            else
+            {
+                label1.Text = label1.Text + "\nERROR: No news credentials found";
+                return false;
+            }
+
+            APIn = new NewsApiClient(API_key);
+            label1.Text = label1.Text + "\nAPI client created";
+
+            return true;
+
+            //return API;
+        }
+
+        public bool ValidateSpotiAPI()
+        {
+
+            string path = FindCredentials();
+
+            if(File.Exists(path))
+            {
+                string API_Key = File.ReadLines(path).Skip(2).Take(1).First();
+                SpotifyWebAPI APIs = new SpotifyWebAPI
+                {
+                    AccessToken = API_Key,
+                    TokenType = "Bearer"
+                };
+
+            }
+            else
+            {
+                label1.Text = label1.Text + "\nERROR: No Spotify credentials found";
+                return false;
+            }
+
+
+            
+
+
+            return true;
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = label1.Text + "\nButton Clicked";
+
+            /*
+            EverythingRequest request = GenerateEverythingRequest();
+            var response = await APIn.GetEverythingAsync(request);
+            */
+
+            TopHeadlinesRequest request = GenerateHeadlinesRequest();
+            var response = await APIn.GetTopHeadlinesAsync(request);
+
+            //var r = await APIn.GetTopHeadlinesAsync();
+
+            label1.Text += label1.Text + "\nResponse recieved";
+
+            if (response.Status == Statuses.Ok)
+            {
+                Console.WriteLine("Responses: " + response.TotalResults);
+                label1.Text += ("Responses: " + response.TotalResults);
+
+                foreach (var article in response.Articles)
+                {
+                    Console.WriteLine("Title: {0} - ({1})", article.Title, article.PublishedAt);
+                    Console.WriteLine("Author: {0}", article.Author);
+                    Console.WriteLine("Description: {0}", article.Description);
+
+                    string txt = "";
+                    txt += "Title: " + article.Title;
+                    txt += "\nPublished: " + article.PublishedAt;
+                    txt += "\nAuthor: " + article.Author;
+                    txt += "\nDescription: " + article.Description;
+                    txt += "\nSource: " + article.Source.Name + "\n\n";
+
+                    TextBox.Text += txt;
+                }
+            }
+            else
+            {
+                label1.Text = label1.Text + "\nError fetching data" + response.Error.Code;
+                label1.Text = label1.Text + "\n\t" + response.Error.Message;
+
+                TextBox.Text = label1.Text + "\nError fetching data" + response.Error.Code;
+                TextBox.Text = label1.Text + "\n\t" + response.Error.Message;
+            }
+
+
+            label1.Text = label1.Text + "\nButton push end";
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (Check_ToDate.Checked) { ToDate.Enabled = true; }
+            else { ToDate.Enabled = false; }
+
+        }
+
+        private void Check_FromDate_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (Check_FromDate.Checked) { FromDate.Enabled = true; }
+            else { FromDate.Enabled = false; }
+
+        }
+
+        private void Check_Country_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (Check_Country.Checked) { Country.Enabled = true; }
+            else { Country.Enabled = false; }
+
+        }
+
+
+
+
+        private string FindCredentials()
+        {
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in drives)
+            {
+                if (d.IsReady)
+                {
+                    string Path = d.RootDirectory + @"SE\credentials.txt";
+
+                    if (File.Exists(Path))
+                    {
+                        return Path;
+                    }
+                }
+            }
+
+            return "";
+
+        }
+
+        private void SetDefaults()
+        {
+
+            ToDate.MaxDate = DateTime.Now;
+            ToDate.MinDate = DateTime.Now.AddMonths(-1);
+            ToDate.Value = ToDate.MaxDate;
+
+            FromDate.MaxDate = DateTime.Now.AddDays(-1);
+            FromDate.MinDate = DateTime.Now.AddMonths(-1);
+            FromDate.Value = FromDate.MinDate;
+
+            Check_ToDate.Checked = true;
+            Check_FromDate.Checked = true;
+            Check_Country.Checked = true;
+            Check_Catagory.Checked = true;
+
+            Country.SelectedItem = "UK";
+
+            Catagory.SelectedItem = "General";
+
+        }
+
+
+        private EverythingRequest GenerateEverythingRequest()
+        {
+
+            EverythingRequest request = new EverythingRequest();
+
+            request.Q = "spotify";
+            request.Language = Languages.EN;
+            request.SortBy = SortBys.PublishedAt;
+
+            if(Check_ToDate.Enabled && Check_FromDate.Enabled)
+            {
+                if (ToDate.Value.Date < FromDate.Value.Date)
+                {
+                    ToDate.Value = ToDate.MaxDate;
+                }
+            }
+
+            if (Check_FromDate.Checked){ request.From = FromDate.Value.Date; }
+            if (Check_ToDate.Checked){ request.To = ToDate.Value.Date; }
+
+            return request;
+
+        }
+
+        private TopHeadlinesRequest GenerateHeadlinesRequest()
+        {
+
+            TopHeadlinesRequest request = new TopHeadlinesRequest();
+
+
+            if(Check_Country.Checked)
+            {
+                switch(Country.SelectedItem.ToString().ToUpper())
+                {
+                    case ("UAE"): request.Country = Countries.AE; break;
+                    case ("ARGENTINA"): request.Country = Countries.AR; break;
+                    case ("AUSTRIA"): request.Country = Countries.AT; break;
+                    case ("AUSTRALIA"): request.Country = Countries.AU; break;
+                    case ("BELGIUM"): request.Country = Countries.BE; break;
+                    case ("BULGARIA"): request.Country = Countries.BG; break;
+                    case ("BRAZIL"): request.Country = Countries.BR; break;
+                    case ("CANADA"): request.Country = Countries.CA; break;
+                    case ("SWITZERLAND"): request.Country = Countries.CH; break;
+                    case ("CHINA"): request.Country = Countries.CN; break;
+                    case ("COLUMBIA"): request.Country = Countries.CO; break;
+                    case ("CUBA"): request.Country = Countries.CU; break;
+                    case ("CZECHIA"): request.Country = Countries.CZ; break;
+                    case ("GERMANY"): request.Country = Countries.DE; break;
+                    case ("EGYPT"): request.Country = Countries.EG; break;
+                    case ("FRANCE"): request.Country = Countries.FR; break;
+                    case ("UK"): request.Country = Countries.GB; break;
+                    case ("GREECE"): request.Country = Countries.GR; break;
+                    case ("HONG KONG"): request.Country = Countries.HK; break;
+                    case ("HUNGARY"): request.Country = Countries.HU; break;
+                    case ("INDONESIA"): request.Country = Countries.ID; break;
+                    case ("IRELAND"): request.Country = Countries.IE; break;
+                    case ("ISRAEL"): request.Country = Countries.IL; break;
+                    case ("INDIA"): request.Country = Countries.IN; break;
+                    case ("ITALY"): request.Country = Countries.IT; break;
+                    case ("JAPAN"): request.Country = Countries.JP; break;
+                    case ("KOREA"): request.Country = Countries.KR; break;
+                    case ("LITHUANIA"): request.Country = Countries.LT; break;
+                    case ("LATVIA"): request.Country = Countries.LV; break;
+                    case ("MOROCCO"): request.Country = Countries.MA; break;
+                    case ("MEXICO"): request.Country = Countries.MX; break;
+                    case ("MALAYSIA"): request.Country = Countries.MY; break;
+                    case ("NIGERIA"): request.Country = Countries.NG; break;
+                    case ("NETHERLANDS"): request.Country = Countries.NL; break;
+                    case ("NORWAY"): request.Country = Countries.NO; break;
+                    case ("NEW ZEALAND"): request.Country = Countries.NZ; break;
+                    case ("PHILIPPINES"): request.Country = Countries.PH; break;
+                    case ("POLAND"): request.Country = Countries.PL; break;
+                    case ("PORTUGAL"): request.Country = Countries.PT; break;
+                    case ("ROMANIA"): request.Country = Countries.RO; break;
+                    case ("SERBIA"): request.Country = Countries.RS; break;
+                    case ("RUSSIA"): request.Country = Countries.RU; break;
+                    case ("SAUDI ARABIA"): request.Country = Countries.SA; break;
+                    case ("SWEDEN"): request.Country = Countries.SE; break;
+                    case ("SLOVENIA"): request.Country = Countries.SI; break;
+                    case ("SLOVAKIA"): request.Country = Countries.SK; break;
+                    case ("THAILAND"): request.Country = Countries.TH; break;
+                    case ("TURKEY"): request.Country = Countries.TR; break;
+                    case ("TAIWAN"): request.Country = Countries.TW; break;
+                    case ("UKRAINE"): request.Country = Countries.UA; break;
+                    case ("US"): request.Country = Countries.US; break;
+                    case ("VENEZUELA"): request.Country = Countries.VE; break;
+                    case ("SOUTH AFRICA"): request.Country = Countries.ZA; break;
+                    default: request.Country = Countries.GB; break;
+                }
+            }
+
+            if(Check_Catagory.Checked)
+            {
+
+                switch (Catagory.SelectedItem.ToString().ToUpper())
+                {
+                    case ("BUSINESS"): request.Category = Categories.Business; break;
+                    case ("ENTERTAINMENT"): request.Category = Categories.Entertainment; break;
+                    case ("HEALTH"): request.Category = Categories.Health; break;
+                    case ("SCIENCE"): request.Category = Categories.Science; break;
+                    case ("SPORTS"): request.Category = Categories.Sports; break;
+                    case ("TECHNOLOGY"): request.Category = Categories.Technology; break;
+                    default: request.Category = Categories.Technology; break;
+                }
+
+            }
+
+
+            return request;
+
+        }
+
+
+        private void Disable()
+        {
+
+            button1.Enabled = false;
+
+            FromDate.Enabled = false;
+            ToDate.Enabled = false;
+
+            TextBox.Enabled = false;
+
+            Check_FromDate.Enabled = false;
+            Check_ToDate.Enabled = false;
+            Check_Country.Enabled = false;
+            Check_Catagory.Enabled = false;
+
+            Country.Enabled = false;
+
+            Catagory.Enabled = false;
+
+        }
+
+        private async void spotifyTest()
+        {
+
+            if(APIs == null)
+            {
+                TextBox.Text += "\nSPOTIFY API NULL";
+            }
+
+            //TextBox.Text += "\n" + APIs.AccessToken;
+            /*
+            PrivateProfile profile = await APIs.GetPrivateProfileAsync();
+            if (!profile.HasError())
+            {
+                TextBox.Text += profile.DisplayName;
+            }
+            */
+        }
+    }
+}
